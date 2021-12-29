0a5c467236a34e6f526b4b2c0804c32de902a3a6
Zohaib Azam <zohaibazam58@gmail.com>
2021-12-30 01:22:21 +0500

    To resolve error .done

---
+++ ToDo/dist/index.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const itemCollection_1 = require("./itemCollection");
+let itemColl = new itemCollection_1.itemCollection("Zohaib");
+itemColl.addToDo("Task1");
+itemColl.addToDo("Task2");
+itemColl.addToDo("Task3");
+itemColl.addToDo("Task4");
+itemColl.taskDone(3);
+itemColl.printAll();
